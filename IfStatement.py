6cbcc3fb8559ff274b81b6e0168e03ad20de2e74
acc0050 <acc0050@mix.wvu.edu>
2022-09-07 16:06:57 -0400

    update

--- IfStatement.py
+++ IfStatement.py
@@ -1,10 +1,16 @@
 """this is an example of If Statements"""
 
-number1 = 12
+number1 = 6
 
 number2 = 6
 
 if number1 > number2:
     print("The values of the first number {0}".format(number1),"is greater than the value of the second number {0}".format(number2))
+
+
+elif number1 < number2:
+    print("The values of the first number {0}".format(number1),"is less than the value of the second number {0}".format(number2))
+
 else:
-    print("The value of the second number is less than the value of the first number")
+    print("The values are equal")
+
