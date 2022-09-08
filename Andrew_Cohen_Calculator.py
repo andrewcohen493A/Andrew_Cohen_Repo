e67602c47bd675a1cf9e4d0ae32767f2866732d1
acc0050 <acc0050@mix.wvu.edu>
2022-09-08 09:39:05 -0400

    updated file

--- Andrew_Cohen_Calculator.py
+++ Andrew_Cohen_Calculator.py
@@ -7,8 +7,8 @@ num1 = 0
 num2 = 0
 
 
-num1 = int(input("What is the value of the first value? "))
+num1 = int(input("What is your first number? "))
 
-num2 = int(input("What is the value of the second value? "))
+num2 = int(input("What is your second number? "))
 
 print("The sum of the two provided values {0}".format(num1),"and {0}".format(num2)," is {0}".format(num1+num2))
