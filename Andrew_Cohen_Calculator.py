40ad3b5859dab2f6984d7f04b08dc4afc710d746
acc0050 <acc0050@mix.wvu.edu>
2022-09-08 15:03:26 -0400

    Simple Calc

--- Andrew_Cohen_Calculator.py
+++ Andrew_Cohen_Calculator.py
@@ -11,4 +11,19 @@ num1 = int(input("What is your first number? "))
 
 num2 = int(input("What is your second number? "))
 
-print("The sum of the two provided values {0}".format(num1),"and {0}".format(num2)," is {0}".format(num1+num2))
+operation = str(input("Please enter a math operation + - * /: "))
+
+if operation == '+':
+    print("The sum of the two provided values {0}".format(num1),"and {0}".format(num2)," is {0}".format(num1+num2))
+
+elif operation == '-':
+    print("The subtraction of the two provided values {0}".format(num1),"and {0}".format(num2)," is {0}".format(num1-num2))
+
+elif operation == '*':
+    print("The multiplication of the two provided values {0}".format(num1),"and {0}".format(num2)," is {0}".format(num1*num2))
+
+elif operation == '/':
+    print("The division of the two provided values {0}".format(num1),"and {0}".format(num2)," is {0}".format(num1/num2))
+
+else:
+    print("No function was entered")
