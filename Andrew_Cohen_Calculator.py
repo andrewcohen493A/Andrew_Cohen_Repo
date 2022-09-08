25dc5001119f66c817f5505a40f83348a4d68523
acc0050 <acc0050@mix.wvu.edu>
2022-09-08 09:35:25 -0400

    updated file

--- Andrew_Cohen_Calculator.py
+++ Andrew_Cohen_Calculator.py
@@ -2,3 +2,13 @@
 Andrew Cohen
 simple calc
 """
+
+num1 = 0
+num2 = 0
+
+
+num1 = int(input("What is the value of the first value? "))
+
+num2 = int(input("What is the value of the second value? "))
+
+print("The sum of the two provided values {0}".format(num1),"and {0}".format(num2)," is {0}".format(num1+num2))
