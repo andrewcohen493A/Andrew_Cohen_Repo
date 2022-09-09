43545d41e21ca0e99cabd2dbb429cb4bae884635
acc0050 <acc0050@mix.wvu.edu>
2022-09-09 16:25:28 -0400

    FancyishCalc

--- FancyCalculator.py
+++ FancyCalculator.py
@@ -1,1 +1,25 @@
-"""Andrew Cohen Fancy Calc"""
+"""
+Andrew Cohen
+this is a Fancy ish Calc
+accepts one user input, the equation and prints the output
+"""
+
+#display message to user
+print("Hello user, this is a calculator")
+
+equation= input("provide me with equation:")
+print("Answer: " + str(eval(equation)))
+
+#val1 = equation.split(' ')[0]
+#op = equation.split(' ')[1]
+#val2 = equation.split(' ')[2]
+
+"""
+When searching i came across this solution, but i am stuck on how to pull the users input. My code works but if i was
+asked to provide the numbers that were inputed i cant get it to work. Going to push the work today but before monday gonna
+try to see if i can get it to work.
+"""
+
+
+
+
