dc4c257730e079b5c80f7cdec691ba22612de581
acc0050 <acc0050@mix.wvu.edu>
2022-09-14 16:06:03 -0400

    update of circle program on pulling methods

--- CircleProgram.py
+++ CircleProgram.py
@@ -3,14 +3,24 @@ Andrew Cohen
 '''
 import math
 
+def CircleArea(diameter):
 
-def ShowCircleArea():
+    radius = .5 * diameter
+
+    area = radius * math.pi
+    return area
+
+def ShowCircleArea(diameter):
 
     diameter = float(input("what is the diameter"))
     # the area is radius (square) * pie
     # let us calculate radius:
 
-    radius = .5 * diameter
-    area = radius * math.pi
+    # radius = .5 * diameter
+    #
+    #
+    # area = radius * math.pi
+    # return area
 
-    print("The area of a circle of {0}".format(diameter),"is {0}".format(area))
+    #print("The area of a circle of {0}".format(diameter),"is {0}".format(area))
+
--- CircleScreen.py
+++ CircleScreen.py
@@ -1,13 +1,13 @@
+
 import CircleProgram
 
 def main():
-    # thediameter = 18.9
-    #
-    # thearea= CircleProgram.CircleArea(thediameter)
-    #
-    # print("the area of a circle {0}".format(thediameter),"is {0}".format(thearea))
-    #
-    # CircleProgram.ShowCircleArea()
+    thediameter = 10.9
 
-    if __name__ =="__main__":
-        main()
+    theArea= CircleProgram.CircleArea(thediameter)
+
+    print("the area of a circle {0}".format(thediameter),"is {0}".format(theArea))
+
+    #CircleProgram.ShowCircleArea()
+if __name__ =="__main__":
+    main()
