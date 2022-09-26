26d3791418e94e2834ffabf55d478c95bd153de0
acc0050 <acc0050@mix.wvu.edu>
2022-09-26 15:55:09 -0400

    Updated my HW_1_Main_Screen to call the new .py files made

--- Homeworks/HW1_Main_Screen.py
+++ Homeworks/HW1_Main_Screen.py
@@ -2,19 +2,20 @@
 Andrew Cohen
 Main screen to call each file
 """
-from In_Class_Activities import FancyCalculator
+from In_Class_Activities import FancyCalculator, Activity_3B
+from Homeworks import Homework_1
 
 
 def main():
     program = int(input("Hello, Which program would you like to use Simple Calc(1), Fancy Calc(2), or Password Checker(3): "))
 
     if program == 1:
-        Andrew_Cohen_Calculator.Simple_Calculator()     #set program equal too a number to call a specific program they are trying to use
+        Activity_3B.Simple_Calculator()     #set program equal too a number to call a specific program they are trying to use
     elif program == 2:                                  #By also creating if else to run through the 3 different files
         FancyCalculator.Fancy_Calculator()
     elif program == 3:                              #when picking 3 user is given a message to input their password
         password = input("Please enter a password with at least 8 characters, symbol(!@#$%), and a digit: ")
-        PasswordChecker.Correct_Password_Response(password) #calls the method and runs through the if statement we made before
+        Homework_1.Correct_Password_Response(password) #calls the method and runs through the if statement we made before
         print(password)
 
 if __name__ == "__main__":
